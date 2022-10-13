0b0b4153b8f4eb4d217ba9faab838b177a971621
Raymond Mulligan <rmulligan@singular.co.za>
2022-10-13 10:17:32 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,17 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {   
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");   
+                    //policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000/").AllowCredentials();   
+                    //policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://172.16.28.83:3000");                                         
+                    //policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+                });
+            });            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +63,9 @@ namespace API
 
             //app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting();            
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
