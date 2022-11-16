1e5860cb60768ccb1095c5363161c6bc887c195c
Raymond Mulligan <rmulligan@singular.co.za>
2022-11-16 14:34:25 +0200

    End of Section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,107 +1,32 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashBard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
-
-  const [activities, setActivties] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
+  const {activityStore} = useStore();
 
   useEffect(() => {
-    agent.Activities.list().then(response => {  
-      let activities : Activity[] = [];     
-      response.forEach (activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-      setActivties(activities);
-      setLoading(false);
-    })   
-  },[])
+    activityStore.loadActivities();
+  },[activityStore])
 
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id))
-  }
-
-  function handleCancelSelectedActivity(){
-    setSelectedActivity(undefined);
-  }
-
-  function handelFormOpen(id?: string){
-    id ? handleSelectActivity(id) : handleCancelSelectedActivity();
-    setEditMode(true);
-  }
-
-  function handelFormClose(){
-    setEditMode(false);
-  }
-
-  function handelCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then( () => {
-        setActivties([...activities.filter(x => x.id !== activity.id), activity]) 
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivties([...activities, activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-    // activity.id 
-    //   ?setActivties([...activities.filter(x => x.id !== activity.id), activity]) 
-    //   :setActivties([...activities, {...activity, id:uuid()}]);
-    //   setEditMode(false);
-    //   setSelectedActivity(activity);
-  }
-
-  function handelDeleteActivity(id: string){
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivties([...activities.filter(x => x.id !== id)])
-      setSubmitting(false);
-    })
-  }
-
-  if (loading) return <LoadingComponent content='Loading App' />
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
 
   return (
     <>
-      <NavBar openForm={handelFormOpen} />
+      <NavBar/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashBard 
-        activities={activities} 
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectedActivity={handleCancelSelectedActivity}
-        editMode={editMode}
-        openForm={handelFormOpen}
-        closeForm={handelFormClose}
-        createOrEdit={handelCreateOrEditActivity}
-        deleteActivity={handelDeleteActivity}
-        submitting={submitting}
-        />
+        <ActivityDashBard />
       </Container>        
     </>
   );
 }
 
-export default App;
+export default observer(App);
 
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,14 @@
+import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
 
-interface Props {
-    activity : Activity | undefined;    
-    closeForm: () => void;
-    createOrEdit : (activity : Activity) => void;
-    submitting : boolean;
-}
 
-export default function ActivityForm({activity : selectedActivity, closeForm, createOrEdit, submitting}:Props) {
+
+export default observer ( function ActivityForm() {
+
+    const {activityStore} = useStore();
+    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
 
     const innitialState = selectedActivity ?? {
         id: '',
@@ -24,7 +23,7 @@ export default function ActivityForm({activity : selectedActivity, closeForm, cr
     const [activity, setActivity] = useState(innitialState);
 
     function handelSubmit () {
-        createOrEdit(activity);        
+       activity.id ? updateActivity(activity) : createActivity(activity);
     }
 
 
@@ -33,6 +32,8 @@ export default function ActivityForm({activity : selectedActivity, closeForm, cr
         setActivity({...activity, [name]:value})
     }
 
+    
+    
     return (
         <Segment clearing>
             <Form onSubmit={handelSubmit} autoComplete='off'>
@@ -42,9 +43,9 @@ export default function ActivityForm({activity : selectedActivity, closeForm, cr
                 <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handelInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handelInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handelInputChange}/>
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />                
             </Form>
         </Segment>
     )
-}
+})
