8fd2820d216472646da14540da8d6d07348d2255
Raymond Mulligan <rmulligan@singular.co.za>
2022-11-22 08:34:27 +0200

    Fix main page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -5,12 +5,12 @@ import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 export default function HomePage() {
     return (
         <Segment inverted textAlign='center' vertical className='masthead'>
-            <Container text>
+            <Container  text>
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
                     Reactivities
                 </Header>
-                <Header as='h2' inerted content='Welcome to Reactivities' />
+                <Header as='h2' inverted content='Welcome to Reactivities' />
                 <Button as={Link} to='/activities' size='huge' inverted>
                     Take me to the Activities.
                 </Button>
